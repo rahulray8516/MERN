e49b7c7c49a76e8b632f5837a3da60e41fdf0092
rahulray8516 <ray.rahul.8101@gmail.com>
2024-03-10 10:23:19 +0530

    commit

--- 100x/02-medium-course-selling-app-admin-dashboard/backend/index.cjs
+++ 100x/02-medium-course-selling-app-admin-dashboard/backend/index.cjs
@@ -1,19 +1,21 @@
 const express = require('express')
-const cors = require('cors')
-const  bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const port = 3001
 const mongoose = require('mongoose')
+
 const app = express()
-app.use(cors) 
 app.use(bodyParser.json())
+app.use(cors())
 
 
 //mongoose Connection
 const connectionString = "mongodb+srv://rahulray8518:rahulray85188101@cluster0.oveeh21.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(connectionString)
-mongoose.connection.on('connected', ()=> {
+mongoose.connection.on('connected', () => {
     console.log("mongoose Connected")
 })
-mongoose.connection.on('disconnected', ()=>{
+mongoose.connection.on('disconnected', () => {
     console.log("mongoose DisConnected")
 })
 mongoose.connection.on('error', (err)=> {
@@ -26,21 +28,11 @@ app.get('/courses',async(req,res)=>{
 
 })
 app.post('/createCourse', async (req, res) => {
-    try {
-        const courseRequestBody = req.body;
-        const courseFinalBody = new courses(courseRequestBody); // Directly pass the request body
-
-        await courseFinalBody.save(); // Using async/await for consistency
-        console.log("Data Saved Successfully!");
-        res.status(201).send(courseFinalBody);
-    } catch (error) {
-        console.error(error); // More detailed error logging
-        res.status(500).send({ message: "Error in Saving Data", error: error.message });
-    }
+    res.send("Connected Here")
 });
 
-app.listen(3002,"100.93.3.137",() => {
-    console.log(`Backend is running on PORT : 3002`)
+app.listen(port,"100.93.3.137",() => {
+    console.log(`Backend is running on PORT : ${port}`)
 })
 
 
