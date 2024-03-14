7fa1787fad5fc85e0fdf17d4be478e054565717a
rahulray8516 <ray.rahul.8101@gmail.com>
2024-03-14 15:57:48 +0530

    commit

--- 100x/02-medium-course-selling-app-admin-dashboard/src/components/Dashboard.jsx
+++ 100x/02-medium-course-selling-app-admin-dashboard/src/components/Dashboard.jsx
@@ -8,7 +8,6 @@ function Dashboard() {
     },[])
 
     const fetchCourses = async() => {
-        
         const response = fetch('http://100.93.3.137:3001/courses/courses');
         const data = await response.json()
         if(response.status === 200){
