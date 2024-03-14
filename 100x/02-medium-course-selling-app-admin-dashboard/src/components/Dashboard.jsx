11edc8a6e3e4e84791a789f44927c7d6f22498b4
rahulray8516 <ray.rahul.8101@gmail.com>
2024-03-14 13:17:25 +0530

    commit

--- 100x/02-medium-course-selling-app-admin-dashboard/src/components/Dashboard.jsx
+++ 100x/02-medium-course-selling-app-admin-dashboard/src/components/Dashboard.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
 
 function Dashboard() {
+    const [courses,setCourses] = useState([])
+
+    useEffect(()=> {
+        fetchCourses
+    },[])
+
+    const fetchCourses = async() => {
+        
+        const response = fetch('http://100.93.3.137:3001/courses/courses');
+        const data = await response.json()
+        if(response.status === 200){
+            setCourses(data)
+            console.log(JSON.stringify(data))
+        }
+    } 
+
     return  (
         <div className="dashboard">
             hi
