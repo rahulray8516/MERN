2157a6b1f28d64fcac65135d862cad73b35b7947
rahulray8516 <ray.rahul.8101@gmail.com>
2024-03-18 15:10:13 +0530

    commit

--- 100x/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ 100x/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -5,7 +5,8 @@ import CreateCourse from './components/CreateCourse';
 import Register from './components/Register';
 import ShowCourses from './components/ShowCourses';
 import Dashboard from './components/Dashboard';
-  // This file shows how you can do routing in React.
+import SignUp from './components/imported/signup/SignUp';
+   // This file shows how you can do routing in React.
 // Try going to /login, /register, /about, /courses on the website and see how the html changes
 // based on the route.
 // You can also try going to /random and see what happens (a route that doesnt exist)
@@ -13,14 +14,14 @@ function App() {
     return (
         <>
          <Router>
-        
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/createCourse" element={<CreateCourse />} />
-                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>} />
                 <Route path="/courses" element={<ShowCourses />} />
+                <Route path='/signup' element={<SignUp/>} />
             </Routes>
         </Router>
         </>
